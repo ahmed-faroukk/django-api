ec0e73fa1633a12641b9d5b543b2ba2d20f90cfb
Ahmed Farouk <72602749+ahmed-faroukk@users.noreply.github.com>
2022-12-17 16:28:43 +0200

    Delete products/from django.urls import path from . import views  urlpatterns = [     path('', views.ProductList.as_view(), name='product_list'),     path('<int:pk> directory

--- products/from django.urls import path from . import views  urlpatterns = [     path('', views.ProductList.as_view(), name='product_list'),     path('<int:pk>/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.ProductList.as_view(), name='product_list'),
-    path('<int:pk>/', views.ProductDetail.as_view(), name='product_detail'),
-]
